f62bbf3b1c3bd5080a85ca4a50405d284dc5df19
morteza <hajiabadi1377@email.kntu.ac.ir>
2024-03-04 02:04:15 +0330

    initial GUI

--- main.py
+++ main.py
@@ -8,34 +8,38 @@ class Messenger(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Peer to Peer Messenger")
-        self.geometry("800x800")
+        
+        self.rowconfigure([0,1], minsize=400, weight=1)
+        self.columnconfigure([0,1], minsize=400, weight=1)
+        
+        self.frm_new_message = tk.Frame(self, relief=tk.RAISED, bd=2)
+        self.frm_received_messages = tk.Frame(self, relief=tk.RAISED, bd=2)
+        self.frm_history = tk.Frame(self, bg="yellow", relief=tk.RAISED, bd=2)
+        
+        self.lbl_received = tk.Label(self.frm_received_messages, text="received_messages")
+        self.lbl_history = tk.Label(self.frm_history, text="", bg="yellow")
+        self.txt_message = tk.Text(self.frm_new_message)
+        self.btn_send = tk.Button(self.frm_new_message, text="Send Message", command=None)
+        self.btn_history = tk.Button(self.frm_history, text="Show History", command=None)
+        
+        self.frm_history.rowconfigure(0, minsize=380, weight=1)
+        self.frm_history.columnconfigure(0, minsize=280, weight=1)
+        
+        self.lbl_received.grid(row=0, column=0, padx=5, pady=5, sticky="nw")
+        self.lbl_history.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
+        self.txt_message.grid(row=0, column=0)
+        self.btn_send.grid(row=1, column=0, sticky="ew")
+        self.btn_history.grid(row=1, column=0, sticky="ew")                
+        
+        self.frm_received_messages.grid(row=0, column=0, sticky="news")
+        self.frm_history.grid(row=1, column=0, sticky="news")
+        self.frm_new_message.grid(row=0, column=1, sticky="ew")
+        
 
-        self.messages = tk.Text(self, 
-                                wrap=tk.WORD, 
-                                bg="yellow",
-                                bd=3,
-                                cursor='arrow',
-                                fg="blue",
-                                tabs=30)
-        self.messages.pack(pady=10)
-
-        self.input_field = tk.Text(self, height=3, wrap='word')
-        self.input_field.pack(pady=10)
-
-        self.send_button = tk.Button(self, text="Send", command=self.send_message)
-        self.send_button.pack(pady=10)
-
-        self.messages.config(state=tk.DISABLED)
-
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind(('', 8080))
-        self.server.listen(1)
-
-        self.receive_thread = threading.Thread(target=self.receive_messages)
-        self.receive_thread.start()
-
-        self.conversation = []
-
+        
+        
+        
+        
     def send_message(self):
         message = self.input_field.get()
         if message:
